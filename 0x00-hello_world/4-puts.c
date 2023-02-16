8bcda0f1d8a829feed3407d259603daa6393544a
MissDianaFlora <dianaflora94@gmail.com>
2023-02-16 11:21:14 -0800

    using betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char Mystr[] = "Programming is like building a multilingual puzzle";
-	puts(Mystr);
-	
-	return 0;
+	puts("\""Programming is like building a multilingual puzzle");
+	return(0);
 }
