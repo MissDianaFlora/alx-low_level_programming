12b4327cff9135e8b5bfafc04c17128bb89b68c6
MissDianaFlora <dianaflora94@gmail.com>
2023-02-23 00:36:40 -0800

    leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -4,7 +4,7 @@
 /**
  * main - takes a date and prints how many days are left in the year, taking
  * leap years into account
- * Return: 
+ * Return: 0
  */
 
 int main(void)
@@ -13,15 +13,11 @@ int main(void)
 	int day;
 	int year;
 
-	month = 4;
-	day = 01;
-	year = 1997;
-	
+	month = 2;
+	day = 29;
+	year = 2000;
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	
 	day = convert_day(month, day);
-	
 	print_remaining_days(month, day, year);
-	
 	return (0);
 }
