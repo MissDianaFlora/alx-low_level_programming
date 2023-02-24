0a50ea5f9425de4bf19a9b19fa11d44f5bd10b47
MissDianaFlora <dianaflora94@gmail.com>
2023-02-24 03:48:54 -0800

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,9 +2,8 @@
 /**
  * print_line - a function that draws a straight line
  * @n: the number of times the character _ should be printed
- * line should end with \n
- *
  */
+
 void print_line(int n)
 {
 	int line;
