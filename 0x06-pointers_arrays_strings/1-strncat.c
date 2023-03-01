479340a4a4a8af485c83b099c4a83d6317312091
MissDianaFlora <dianaflora94@gmail.com>
2023-03-01 08:28:06 -0800

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,29 @@
 #include "main.h"
 /**
- * strncat - concatenates the string pointed to by @src,
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
+ * _strncat - concatenates two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: A pointer to the destination string @dest.
+ * Return: dest.
  */
-char *_strncpy(char *dest, char *src, int n)src)
+char *_strncat(char *dest, char *src, int n)
 {
-        int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-        while (dest[index++])
-                dest_len++;
-
-        for (index = 0; scr[index]; index++)
-                dest[dest_len++] = src[index];
-
-        return (dest);
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0\;
+	return (dest);
 }
